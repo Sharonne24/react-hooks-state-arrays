cb836c43bbd8e60e65802e7076e8f808a7ea3d5b
Sharon Awuor <sawuor395@gmail.com>
2023-05-21 15:45:04 +0300

    completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -2,19 +2,49 @@ import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
-  const [foods, setFoods] = useState(spicyFoods);
+  const [foods, setFoods] = useState(spicyFoods); // state variable declaration
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray);
     console.log(newFood);
   }
 
+  function handleLiClick(id) {
+    const newFoodArray = foods.map((food) => {
+      if (food.id === id) {
+        return {
+          ...food,
+          heatLevel: food.heatLevel + 1,
+        };
+      } else {
+        return food;
+      }
+    });
+    setFoods(newFoodArray);
+  }
+
+  // function handleFilterChange(event) {
+  //   setFilterBy(event.target.value);
+  // }
+
+  // const foodsToDisplay = foods.filter((food) => {
+  //   if (filterBy === "All") {
+  //     return true;
+  //   } else {
+  //     return food.cuisine === filterBy;
+  //   }
+  // });
+
+  // use of li element to display foods
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)} >
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
+  //adding new spicy food to the array
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
@@ -22,5 +52,20 @@ function SpicyFoodList() {
     </div>
   );
 }
+
+// return (
+//   <div>
+//     <button onClick={handleAddFood}>Add New Food</button>
+//     <select name="filter" onChange={handleFilterChange}>
+//       <option value="All">All</option>
+//       <option value="American">American</option>
+//       <option value="Sichuan">Sichuan</option>
+//       <option value="Thai">Thai</option>
+//       <option value="Mexican">Mexican</option>
+//     </select>
+//     <ul>{foodList}</ul>
+//   </div>
+// );
+// }
 
 export default SpicyFoodList;
